7989aa8f68abbfec0a7326106af99a92b25d466a
Lynett Maina <lynnetnyambura2005@gmail.com>
2025-02-05 03:47:23 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function displayDateTime() {
+  document.getElementById('demo').innerHTML = Date();
+}
+
+function myFunction() {
+  document.getElementById("demo2").innerHTML = "Paragraph changed.";
+}
+
+function myFunctionInHead() {
+  document.getElementById("demo3").innerHTML = "Paragraph changed in head.";
+}
